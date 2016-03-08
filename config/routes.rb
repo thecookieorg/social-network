a45b432e099c406432a4308bfa80d8d1ff74ad16
Marko Manojlovic <marko@thecookieapps.com>
2016-03-08 22:24:30 +0000

    Added a user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: 'registrations'}
+    devise_for :users, controllers: {registrations: 'registrations'}
   get 'pages/home'
+  resources :users, only: [:show]
   root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
